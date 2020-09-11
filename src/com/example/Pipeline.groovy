a946e1c3df42be263042c261ba2ba33c5f62a849
Dmitry <dimitrez@gmail.com>
2020-09-11 13:50:26 +0300

    test

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -1,4 +1,5 @@
 package com.example
+import org.yaml.snakeyaml.*
 
 class Pipeline {
     def script
@@ -13,6 +14,9 @@ class Pipeline {
 //    ===================== Your Code Starts Here =====================
 //    Note : use "script" to access objects from jenkins pipeline run (WorkflowScript passed from Jenkinsfile)
 //           for example: script.node(), script.stage() etc
+        //Yaml yaml = new Yaml();
+        //def document = new FileReader("config.yml");
+        println(yaml.load(configurationFile));
 
 //    ===================== Parse configuration file ==================
 
