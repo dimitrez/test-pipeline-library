4d35719a4fc3ad48c1a839bc969f8fc01d6852dc
Dmitry <dimitrez@gmail.com>
2020-09-13 00:56:31 +0300

    sh

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -37,7 +37,7 @@ class Pipeline {
 //
         String deploy = config['deploy']['deployCommand'].toString()
 //
-        def testsFolder = config['test']['name']['performance']['testFolder']
+        def testsFolder = config['test']['testFolder']['name']['performance']
 //
 //        String performanceTestCommand = config['test']['name']['performance']['testCommand'].toString()
 //        String regressionTestCommand  = config['test']['name']['regression']['testCommand'].toString()
